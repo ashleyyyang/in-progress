233a630d2a83cfab06699999a98c8a602455a225
Ashley Yang <43325293+ashleyyyang@users.noreply.github.com>
2019-02-09 04:37:20 -0500

    Add files via upload

--- js/font.js
+++ js/font.js
@@ -5,3 +5,9 @@ jQuery(document).ready(function(){
       $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(255,152,146,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,205,146,1), '+rXP/70+'px '+rYP/12+'px rgba(146,183,255,.7)');
     });
  });
+
+
+ $(".card").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+  });
